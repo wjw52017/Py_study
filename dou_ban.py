3f5e38330171ab2e1e15b1e81c45e9263be6e0b2
wjw52017 <2542464643@qq.com>
2017-06-09 19:26:18 +0800

    Delete

--- dou_ban.py
+++ dou_ban.py
@@ -2,6 +2,8 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+from colorama import init,Fore,Style
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -14,10 +16,13 @@ def getTag(url):
     context = response.read()
 
     soup = BeautifulSoup(context,"html.parser")
+    text = []
     for tag in soup.find_all('table','tagCol'):
         d = tag.get_text()
-        with open("E:\\douban.txt","a") as f:
-            f.write(d)
+        text.append(d)
+    return text
+        #with open("E:\\douban.txt","a") as f:
+            #f.write(d)
 
         #for h in tag.find_all("a"):
             #print h.get("href")
@@ -25,5 +30,19 @@ def getTag(url):
 
 if __name__ == '__main__':
     url = "https://book.douban.com/tag/?icn=index-nav"
-    getTag(url)
+    text = getTag(url)
+
+    #处理中文乱码
+    #rigth_text = str(text).replace('u\'', '\'')
+   # result =  rigth_text.decode("unicode-escape")
+    #print result[]
+
+    x = PrettyTable(['name'])   #输出格式处理
+    x.align["name"] = "l"
+    x.padding_width = 1
+    for r in text:
+        x.add_row([r])
+    init(autoreset=True)
+    print Fore.RED + Style.BRIGHT + '书籍种类 and 数量 '
+    print x
 
